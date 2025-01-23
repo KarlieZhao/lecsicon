5a68da5e9c4204617f5da4d3f4b040c0a5a8f986
Karlie Zhao <62089883+KarlieZhao@users.noreply.github.com>
2025-01-22 21:42:15 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/lecsicon/',
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    base: '/lecsicon/',
+    define: {
+      'process.env.VITE_OPENAI_API_KEY': JSON.stringify(env.VITE_OPENAI_API_KEY)
+    },
+    plugins: [react()],
+  }
 })
